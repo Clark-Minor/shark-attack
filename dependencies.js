d0391408c847a6d4316c1923fc01295b576021b7
shelley <shelleypi@ymail.com>
2019-11-30 11:57:35 -0800

    Finished eyes and fixed lighting on water

--- dependencies.js
+++ dependencies.js
@@ -16,7 +16,7 @@ class Triangle extends Shape    // The simplest possible Shape – one triangle.
     }
 }
 
-window.Triangle2 = window.classes.Triangl2e =
+window.Triangle2 = window.classes.Triangle2 =
 class Triangle2 extends Shape    // The simplest possible Shape – one triangle.  It has 3 vertices, each
 { constructor()                 // having their own 3D position, normal vector, and texture-space coordinate.
     { super( "positions", "normals", "texture_coords" );                       // Name the values we'll define per each vertex.
@@ -33,6 +33,30 @@ class Triangle2 extends Shape    // The simplest possible Shape – one triangle
                  // told it how to connect vertex entries into triangles.  Every three indices in "this.indices" traces out one triangle.
     }
 }
+
+window.Cube = window.classes.Cube =
+class Cube extends Shape                 // Here's a complete, working example of a Shape subclass.  It is a blueprint for a cube.
+  { constructor()
+      { super( "positions", "normals" ); // Name the values we'll define per each vertex.  They'll have positions and normals.
+
+        // First, specify the vertex positions -- just a bunch of points that exist at the corners of an imaginary cube.
+        this.positions.push( ...Vec.cast( [-.1,-.1,-.1], [.1,-.1,-.1], [-.1,-.1,.1], [.1,-.1,.1], [.1,.1,-.1],  [-.1,.1,-.1],  [.1,.1,.1],  [-.1,.1,.1],
+                                          [-.1,-.1,-.1], [-.1,-.1,.1], [-.1,.1,-.1], [-.1,.1,.1], [.1,-.1,.1],  [.1,-.1,-.1],  [.1,.1,.1],  [.1,.1,-.1],
+                                          [-.1,-.1,.1],  [.1,-.1,.1],  [-.1,.1,.1],  [.1,.1,.1], [.1,-.1,-.1], [-.1,-.1,-.1], [.1,.1,-.1], [-.1,.1,-.1] ) );
+        // Supply vectors that point away from eace face of the cube.  They should match up with the points in the above list
+        // Normal vectors are needed so the graphics engine can know if the shape is pointed at light or not, and color it accordingly.
+        this.normals.push(   ...Vec.cast( [0,-1,0], [0,-1,0], [0,-1,0], [0,-1,0], [0,1,0], [0,1,0], [0,1,0], [0,1,0], [-1,0,0], [-1,0,0],
+                                          [-1,0,0], [-1,0,0], [1,0,0],  [1,0,0],  [1,0,0], [1,0,0], [0,0,1], [0,0,1], [0,0,1],   [0,0,1],
+                                          [0,0,-1], [0,0,-1], [0,0,-1], [0,0,-1] ) );
+
+                 // Those two lists, positions and normals, fully describe the "vertices".  What's the "i"th vertex?  Simply the combined
+                 // data you get if you look up index "i" of both lists above -- a position and a normal vector, together.  Now let's
+                 // tell it how to connect vertex entries into triangles.  Every three indices in this list makes one triangle:
+        this.indices.push( 0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
+                          14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22 );
+        // It stinks to manage arrays this big.  Later we'll show code that generates these same cube vertices more automatically.
+      }
+  }
 
 window.Square = window.classes.Square =
 class Square extends Shape              // A square, demonstrating two triangles that share vertices.  On any planar surface, the interior
--- main-scene.js
+++ main-scene.js
@@ -1,16 +1,16 @@
-window.Cube = window.classes.Cube =
-class Cube extends Shape    // A cube inserts six square strips into its arrays.
-{ constructor()
-    { super( "positions", "normals", "texture_coords" );
-      for( var i = 0; i < 3; i++ )
-        for( var j = 0; j < 2; j++ )
-        { var square_transform = Mat4.rotation( i == 0 ? Math.PI/2 : 0, Vec.of(1, 0, 0) )
-                         .times( Mat4.rotation( Math.PI * j - ( i == 1 ? Math.PI/2 : 0 ), Vec.of( 0, 1, 0 ) ) )
-                         .times( Mat4.translation([ 0, 0, 1 ]) );
-          Square.insert_transformed_copy_into( this, [], square_transform );
-        }
-    }
-}
+// window.Cube = window.classes.Cube =
+// class Cube extends Shape    // A cube inserts six square strips into its arrays.
+// { constructor()
+//     { super( "positions", "normals", "texture_coords" );
+//       for( var i = 0; i < 3; i++ )
+//         for( var j = 0; j < 2; j++ )
+//         { var square_transform = Mat4.rotation( i == 0 ? Math.PI/2 : 0, Vec.of(1, 0, 0) )
+//                          .times( Mat4.rotation( Math.PI * j - ( i == 1 ? Math.PI/2 : 0 ), Vec.of( 0, 1, 0 ) ) )
+//                          .times( Mat4.translation([ 0, 0, 1 ]) );
+//           Square.insert_transformed_copy_into( this, [], square_transform );
+//         }
+//     }
+// }
 
 
 //SURFACE OF WATER//
@@ -103,6 +103,30 @@ class Body_Of_Water extends Shape
       }
 }
 
+window.Edge_Of_Tank = window.classes.Edge_Of_Tank =
+class Edge_Of_Tank extends Shape                 // Here's a complete, working example of a Shape subclass.  It is a blueprint for a cube.
+  { constructor()
+      { super( "positions", "normals" ); // Name the values we'll define per each vertex.  They'll have positions and normals.
+
+        // First, specify the vertex positions -- just a bunch of points that exist at the corners of an imaginary cube.
+        this.positions.push( ...Vec.cast( [-1,-1,-1], [1,-1,-1], [-1,-1,1], [1,-1,1], [1,1,-1],  [-1,1,-1],  [1,1,1],  [-1,1,1],
+                                          [-1,-1,-1], [-1,-1,1], [-1,1,-1], [-1,1,1], [1,-1,1],  [1,-1,-1],  [1,1,1],  [1,1,-1],
+                                          [-1,-1,1],  [1,-1,1],  [-1,1,1],  [1,1,1], [1,-1,-1], [-1,-1,-1], [1,1,-1], [-1,1,-1] ) );
+        // Supply vectors that point away from eace face of the cube.  They should match up with the points in the above list
+        // Normal vectors are needed so the graphics engine can know if the shape is pointed at light or not, and color it accordingly.
+        this.normals.push(   ...Vec.cast( [0,-1,0], [0,-1,0], [0,-1,0], [0,-1,0], [0,1,0], [0,1,0], [0,1,0], [0,1,0], [-1,0,0], [-1,0,0],
+                                          [-1,0,0], [-1,0,0], [1,0,0],  [1,0,0],  [1,0,0], [1,0,0], [0,0,1], [0,0,1], [0,0,1],   [0,0,1],
+                                          [0,0,-1], [0,0,-1], [0,0,-1], [0,0,-1] ) );
+
+                 // Those two lists, positions and normals, fully describe the "vertices".  What's the "i"th vertex?  Simply the combined
+                 // data you get if you look up index "i" of both lists above -- a position and a normal vector, together.  Now let's
+                 // tell it how to connect vertex entries into triangles.  Every three indices in this list makes one triangle:
+        this.indices.push( 8, 9, 10, 9, 11, 10, 12, 13,
+                          14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22 );
+        // It stinks to manage arrays this big.  Later we'll show code that generates these same cube vertices more automatically.
+      }
+  }
+
 window.Octopus = window.classes.Octopus =
 class Octopus extends Shape
 { constructor()
@@ -158,6 +182,21 @@ class Octopus_Eyes extends Shape
    }
 }
 
+window.Octopus_Pupil = window.classes.Octopus_Pupil =
+class Octopus_Pupil extends Shape
+{
+  constructor()                 // having their own 3D position, normal vector, and texture-space coordinate.
+   { super( "positions", "normals", "texture_coords" );
+     var eye_1_t = Mat4.identity().times(Mat4.translation(Vec.of(3.5,.1,1))).times(Mat4.scale([2,1.7,3]));
+     var eye_2_t = eye_1_t.times(Mat4.translation(Vec.of(0,.8,0)));
+
+     Cube.insert_transformed_copy_into(this, [], eye_1_t);
+     Cube.insert_transformed_copy_into(this, [], eye_2_t);
+
+
+   }
+}
+
 window.Shark = window.classes.Shark =
 class Shark extends Shape
 {
@@ -167,7 +206,7 @@ class Shark extends Shape
             var head_t = Mat4.identity().times(Mat4.translation([3.2,0,0])).times(Mat4.rotation(Math.PI/2, Vec.of(0,1,0))).times(Mat4.scale([1,1.1,1]))
             var bottom_t = Mat4.identity().times(Mat4.translation([-3,0,0])).times(Mat4.rotation(-Math.PI/2, Vec.of(0,1,0))).times(Mat4.scale([1,1.1,1]))
             var tail_t = Mat4.identity().times(Mat4.translation([-3,0,0])).times(Mat4.rotation(Math.PI/2, Vec.of(1,0,0))).times(Mat4.scale([1.5,1.5,1.5]))
-            var fin_t = Mat4.identity().times(Mat4.translation([0,0,0.3])).times(Mat4.rotation(Math.PI/2, Vec.of(1,0,0))).times(Mat4.scale([2,2,2]))
+            var fin_t = Mat4.identity().times(Mat4.translation([0,0,0.3])).times(Mat4.rotation(Math.PI/2, Vec.of(1,0,0))).times(Mat4.scale([2.2,2.2,2.2]))
             Rounded_Capped_Cylinder.insert_transformed_copy_into(this, [15,15], body_t);
             Rounded_Closed_Cone.insert_transformed_copy_into(this, [15,15], head_t);
             Rounded_Closed_Cone.insert_transformed_copy_into(this, [15,15], bottom_t);
@@ -201,62 +240,6 @@ class Texture_Caustic extends Phong_Shader
 
 
 
-// Extra credit begins here (See TODO comments below):
-
-window.Ring_Shader = window.classes.Ring_Shader =
-class Ring_Shader extends Shader              // Subclasses of Shader each store and manage a complete GPU program.
-{ material() { return { shader: this } }      // Materials here are minimal, without any settings.
-  map_attribute_name_to_buffer_name( name )       // The shader will pull single entries out of the vertex arrays, by their data fields'
-    {                                             // names.  Map those names onto the arrays we'll pull them from.  This determines
-                                                  // which kinds of Shapes this Shader is compatible with.  Thanks to this function,
-                                                  // Vertex buffers in the GPU can get their pointers matched up with pointers to
-                                                  // attribute names in the GPU.  Shapes and Shaders can still be compatible even
-                                                  // if some vertex data feilds are unused.
-      return { object_space_pos: "positions" }[ name ];      // Use a simple lookup table.
-    }
-    // Define how to synchronize our JavaScript's variables to the GPU's:
-  update_GPU( g_state, model_transform, material, gpu = this.g_addrs, gl = this.gl )
-      { const proj_camera = g_state.projection_transform.times( g_state.camera_transform );
-                                                                                        // Send our matrices to the shader programs:
-        gl.uniformMatrix4fv( gpu.model_transform_loc,             false, Mat.flatten_2D_to_1D( model_transform.transposed() ) );
-        gl.uniformMatrix4fv( gpu.projection_camera_transform_loc, false, Mat.flatten_2D_to_1D(     proj_camera.transposed() ) );
-      }
-  shared_glsl_code()            // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
-    { return `precision mediump float;
-              varying vec4 position;
-              varying vec4 center;
-      `;
-    }
-  vertex_glsl_code()           // ********* VERTEX SHADER *********
-    { return `
-        attribute vec3 object_space_pos;
-        uniform mat4 model_transform;
-        uniform mat4 projection_camera_transform;
-
-        void main()
-        {
-        }`;           // TODO:  Complete the main function of the vertex shader (Extra Credit Part II).
-    }
-  fragment_glsl_code()           // ********* FRAGMENT SHADER *********
-    { return `
-        void main()
-        {
-        }`;           // TODO:  Complete the main function of the fragment shader (Extra Credit Part II).
-    }
-}
-
-window.Grid_Sphere = window.classes.Grid_Sphere =
-class Grid_Sphere extends Shape           // With lattitude / longitude divisions; this means singularities are at
-  { constructor( rows, columns, texture_range )             // the mesh's top and bottom.  Subdivision_Sphere is a better alternative.
-      { super( "positions", "normals", "texture_coords" );
-
-
-                      // TODO:  Complete the specification of a sphere with lattitude and longitude lines
-                      //        (Extra Credit Part III)
-      } }
-
-
-
 
 
 
@@ -272,7 +255,12 @@ class Project_Scene extends Scene_Component
 
         //context.globals.graphics_state.camera_transform = Mat4.look_at( Vec.of( 0,0,20 ), Vec.of( 0,0,0 ), Vec.of( 0,1,0 ) );
         this.initial_camera_location = Mat4.inverse( context.globals.graphics_state.camera_transform );
-        context.globals.graphics_state.camera_transform = Mat4.translation([ -5,-5,-70 ]);
+        context.globals.graphics_state.camera_transform = Mat4.translation([ 0,0,-30 ])
+                                                    .times(Mat4.translation([ 0,-10,0 ]))
+                                                    .times(Mat4.translation([ 0,0,0 ]))
+                                                    .times(Mat4.rotation(-1.25, Vec.of(1,0,0)))
+                                                    .times(Mat4.rotation(Math.PI/4, Vec.of(0,0,1)))
+                                                    //.times(Mat4.rotation(Math.PI/4, Vec.of(0,1,0)));
 
         const r = context.width/context.height;
         context.globals.graphics_state.projection_transform = Mat4.perspective( Math.PI/4, r, .1, 1000 );
@@ -289,7 +277,7 @@ class Project_Scene extends Scene_Component
 
         //for text on display
         this.ctx_2d = text_canvas.getContext("2d");
-        this.ctx_drawn = false;
+        this.isGameOver = false;
         this.minutes = 0;
         this.seconds = 0;
         this.timer = setTimeout( () => {this.addTime();}, 1000 );  //adds time after 1s
@@ -348,13 +336,15 @@ class Project_Scene extends Scene_Component
                          //        (Requirement 1)
                          axis: new Axis_Arrows(),
 
-                         water: new Water(this.rows, this.columns, this.width, this.length, this.Height_Map),
+                         water: new ( Water.prototype.make_flat_shaded_version() )(this.rows, this.columns, this.width, this.length, this.Height_Map),
                          caustic: new Square(),
                          tank: new Body_Of_Water(),
-                         skybox: new Cube(),
+                         //skybox: new Cube(),
+                         tankedge: new Edge_Of_Tank(),
 
                          octopus: new Octopus(),
                          eyes: new Octopus_Eyes(),
+                         pupil: new Octopus_Pupil(),
 
                          shark: new Shark(),
 
@@ -371,13 +361,15 @@ class Project_Scene extends Scene_Component
         this.materials =
           { axis_material:     context.get_instance( Phong_Shader ).material(Color.of(1,1,1,1)),
 
-            water_material:     context.get_instance( Phong_Shader ).material(Color.of( /*36/255, 171/255, 255/255,*/56/255, 213/255, 252/255, 0.6 ),
+            water_material:     context.get_instance( Phong_Shader ).material(Color.of( /*36/255, 171/255, 255/255,*/ /*56/255, 213/255, 252/255,*/0,149/255,255/255, 0.6 ),
             {
-              ambient: 0.6,
-              diffusivity: 0.1,
-              specular: 0.5,
+              ambient: 1,  //0.6
+              diffusivity: 0.5,  //0.1
+              specular: 0,  //0.5
               smoothness: 80   //fuck is the difference???
             }),
+
+            edge_material: context.get_instance(Phong_Shader).material(Color.of(0,0,0.5,1), {ambient: 1}),
 
             wall_texture: context.get_instance( Phong_Shader ).material(Color.of(0, 0, 0, 1), //opaque black
             {
@@ -391,14 +383,16 @@ class Project_Scene extends Scene_Component
                 texture: context.get_instance("assets/sand.png", true) //true = trilinear filtering
             }),
 
-            octopus_skin: context.get_instance( Phong_Shader ).material(Color.of(204/255,0,170/255,1),
+            octopus_skin: context.get_instance( Phong_Shader ).material(Color.of(/*204/255,0,170/255*/ 235/255,84/255,150/255,1),
             {
-                ambient: 1,
-                //diffusivity: 1,
-                specular: 0.5,
+                ambient: 0.8,
+                //diffusivity: 0.8,
+                //specular: 0.5,
             }),
 
             eye_material: context.get_instance( Phong_Shader ).material(Color.of(1,1,1,1), {ambient: 0.8}),
+            pupil_material: context.get_instance( Phong_Shader ).material(Color.of(0,0,0,1), {ambient: 0.8}),
+
 
             caustic_material: context.get_instance( Texture_Caustic ).material(Color.of( 0,0,0,1 ),
             {
@@ -413,8 +407,11 @@ class Project_Scene extends Scene_Component
 
           }
 
-        //(position, color, size)
-        this.lights = [ new Light( Vec.of( 5,-10,5,1 ), Color.of( 0, 1, 1, 1 ), 1000 ) ];
+        //(position, color, size) 
+        this.lights = [ //new Light( Vec.of( this.width/2,this.length/2,0,1 ), Color.of( 1, 0.4, 1, 1 ), 100000 ), 
+                        //new Light( Vec.of( this.width/2,this.length/2,0,-1 ), Color.of( 1, 0.4, 1, 1 ), 1000 ),
+                        new Light( Vec.of( 70,95,10,1 ), Color.of( 1, .4, 1, 1 ), 100000 ) 
+                        ];
         //this.lights = [ new Light( Vec.of( 0,0,this.height,1 ), Color.of( 0, 1, 1, 1 ), 1000 ),
         //                new Light( Vec.of( this.width/2,0,this.height,1 ), Color.of( 0, 1, 1, 1 ), 1000 ),
         //                new Light( Vec.of( 0,this.length/2,this.height,1 ), Color.of( 0, 1, 1, 1 ), 1000 ) ];
@@ -549,9 +546,10 @@ class Project_Scene extends Scene_Component
             octo_rect.y + octo_rect.height > shark_rect.y
           && this.octopus_t[2][3] - shark[2] == 3.75){
 
-                //this.ctx_drawn = !this.ctx_drawn;
-                this.graphics_state.animate = 0
-                //this.draw_gameOver();
+                //this.graphics_state.animate = 0
+                this.isGameOver = !this.isGameOver;
+                this.draw_gameOver();
+                clearTimeout(this.timer); 
             }
     }
 
@@ -560,6 +558,7 @@ class Project_Scene extends Scene_Component
 
         this.shapes.octopus.draw(graphics_state, transform, this.materials.octopus_skin);
         this.shapes.eyes.draw(graphics_state, transform, this.materials.eye_material);
+        this.shapes.pupil.draw(graphics_state, transform, this.materials.pupil_material);
 
     }
 
@@ -649,8 +648,8 @@ class Project_Scene extends Scene_Component
 
     draw_gameOver()
     {
-        this.ctx_drawn = true;
-        this.ctx_2d.fillStyle = "rgba(0.5,0.5,0.5,0.5)";
+        this.isGameOver = true;
+        this.ctx_2d.fillStyle = "rgba(0.5,0.5,0.5,0.2)";
         this.ctx_2d.fillRect(0,0,1080,600);
         this.ctx_2d.strokeRect(0,0,1080,600);
         this.ctx_2d.fillStyle="#fff";
@@ -658,7 +657,7 @@ class Project_Scene extends Scene_Component
         this.ctx_2d.textAlign = "center";
         this.ctx_2d.fillText("GAME OVER", 540, 300);
         this.ctx_2d.font = "18px PixelFont";
-        this.ctx_2d.fillText("Press x to restart", 540, 350);
+        this.ctx_2d.fillText("Refresh to restart", 540, 350);
         this.ctx_2d.font = "14px PixelFont";
         this.ctx_2d.fillText("time - " + this.get_timeText(), 540, 400);
         this.ctx_2d.fillText("kills - " + this.shark_kills, 540, 420);
@@ -702,9 +701,9 @@ class Project_Scene extends Scene_Component
 
 
         //CREATE NEW WATER SHAPE
-        this.shapes.water = new Water(this.rows, this.columns, this.width, this.length, this.Height_Map);
+        this.shapes.water = new ( Water.prototype.make_flat_shaded_version() )(this.rows, this.columns, this.width, this.length, this.Height_Map);
         this.shapes.water.send_water(this.gl);
-
+        //this.shapes.tankedge.draw(graphics_state, Mat4.identity().times(Mat4.translation([this.width/2,this.length/2,1.5])).times(Mat4.rotation(Math.PI/2, Vec.of(1,0,0))).times(Mat4.scale([this.width/2, 1.5, this.length/2])), this.materials.edge_material);
 
 
         //this.shapes.skybox.draw(graphics_state, Mat4.identity().times(Mat4.rotation(Math.PI/2, Vec.of(1,0,0))).times(Mat4.scale([500,500,500])), this.materials.box_texture);
@@ -758,7 +757,7 @@ class Project_Scene extends Scene_Component
 
         //ours
         let water_transform = Mat4.identity().times( Mat4.translation([0, 0, 0]) ); //make whole surface go up and down
-        this.shapes.water.draw(graphics_state, water_transform, this.materials.water_material);
+        this.shapes.water.draw(graphics_state, water_transform, this.materials.water_material, "TRIANGLE_STRIP");
         //console.log(this.shapes.water.positions)
 
 
@@ -770,7 +769,7 @@ class Project_Scene extends Scene_Component
 
         this.draw_skybox(graphics_state);
 
-        if(this.octo_sinking && !this.ctx_drawn)
+        if(this.octo_sinking && !this.isGameOver)
         {
             this.draw_gameOver();
             clearTimeout(this.timer);  //stops time
